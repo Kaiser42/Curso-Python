12e9200e8a0b3831eaa528fac100dccd1d4fed76
Kaiser <raid21e4@gmail.com>
2024-06-05 03:13:59 +0200

    Seccion 2,3,4

---
+++ Seccion-03/Variables-1.py
@@ -0,0 +1,5 @@
+nombre = "Juan Perez"
+telefono = 123456789
+email = "HolaMundo@gmail.com"
+
+print('Mi nombre es' + nombre + 'puedes enviarme un correo a ' +email + ' o llamar al ' + str(telefono))
